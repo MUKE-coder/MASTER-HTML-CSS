0b767fc41f14a824a9d592afb74cb8f6417dfa3a
MUKE-coder <gmukejohnbaptist@gmail.com>
2022-08-31 14:48:24 +0300

    added exclusive css

---
+++ exclusive css/Responsive/script.js
@@ -0,0 +1,7 @@
+const options = {
+  strings: ["Freelancer", "Web Developer", "Graphic designer", "Web designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+};
+
+var typed = new Typed(".typing", options);
